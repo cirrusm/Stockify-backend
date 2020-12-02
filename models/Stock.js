a556c5ea74d04af9fd6733dee3edaa5dea9d5785
Cirrus Mokhtari <Cirrus1994@gmail.com>
2020-12-02 13:56:21 -0800

    added validation forms

--- models/Stock.js
+++ models/Stock.js
@@ -12,4 +12,12 @@ const stockSchema = new mongoose.Schema({
     type: Date,
     default: Date.now,
   },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
 });
+
+const Stock = mongoose.model("Stock", stockSchema);
+
+module.exports = Stock;
--- server.js
+++ server.js
@@ -11,10 +11,17 @@ app.use(
 app.use(bodyParser.json());
 // DB Config
 const db = require("./config/keys").mongoURI;
+
 // Connect to MongoDB
 mongoose
-  .connect(db, { useNewUrlParser: true })
+  .connect(db, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  })
   .then(() => console.log("MongoDB successfully connected"))
   .catch((err) => console.log(err));
+
 const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
